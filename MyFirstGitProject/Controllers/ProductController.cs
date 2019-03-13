10460bd78a77db571f02f8df2b69307284d28d42
Priya Rajasekaran <priyarjsekaran@gmail.com>
2019-03-13 07:24:53 -0400

    Updated Product controller with console log

---
+++ MyFirstGitProject/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstGitProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            Console.Write("hello");
+            return View();
+        }
+    }
+}
